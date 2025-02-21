f10498aea7f874b9588889f0a0d4f9b5b4621b25
giorgia <n00230352@iadt.ie>
2025-02-21 09:17:09 +0000

    barchart

---
+++ CA/classes/barChart.js
@@ -0,0 +1,102 @@
+class BarChart {
+    constructor(obj) {
+        this.data = obj.data;
+        this.xValue = obj.xValue;
+        this.yValue = obj.yValue;
+        this.chartHeight = obj.chartHeight || 300;
+        this.chartWidth = obj.chartWidth || 300;
+        this.barWidth = obj.barWidth || 10;
+        this.margin = obj.margin || 10;
+
+        this.axisThickness = obj.axisThickness || 1;
+        this.chartPosX = obj.chartPosX || 50;
+        this.chartPosY = obj.chartPosY || 350;
+
+        this.gap = (this.chartWidth - (this.data.length * this.barWidth) - (this.margin * 2)) / (this.data.length - 1);
+
+        this.scaler = this.chartHeight / (max(this.data.map(row => row[this.yValue])));
+
+        this.axisColour = color(50);
+        this.axisTickColour = color(100);
+        this.barColor = color(30, 60, 120);
+        this.axisTextColour = color(0);
+
+        this.numTicks = 5;
+        this.tickLength = 10;
+    }
+
+    renderBars() {
+        push();
+        translate(this.chartPosX, this.chartPosY);
+        noFill();
+        stroke(this.axisColour);
+        strokeWeight(this.axisThickness);
+        line(0, 0, 0, -this.chartHeight); // Vertical line
+        line(0, 0, this.chartWidth, 0); // Horizontal line
+
+        push();
+        translate(this.margin, 0);
+        for (let i = 0; i < this.data.length; i++) {
+            let xPos = (this.barWidth + this.gap) * i;
+            fill(this.barColor);
+            rect(xPos, 0, this.barWidth, -this.data[i][this.yValue] * this.scaler);
+        }
+        pop();
+        pop();
+    }
+
+    renderAxis() {
+        push();
+        translate(this.chartPosX, this.chartPosY);
+        noFill();
+        stroke(this.axisColour);
+        strokeWeight(this.axisThickness);
+        line(0, 0, 0, -this.chartHeight); // Vertical line
+        line(0, 0, this.chartWidth, 0); // Horizontal line
+        pop();
+    }
+
+    renderLabels() {
+        push();
+        translate(this.chartPosX, this.chartPosY);
+
+        push();
+        translate(this.margin, 0);
+        for (let i = 0; i < this.data.length; i++) {
+            let xPos = (this.barWidth + this.gap) * i;
+
+            fill(this.axisTextColour);
+            noStroke();
+            textAlign(LEFT, CENTER);
+            textSize(12);
+            push();
+            translate(xPos + this.barWidth / 2, 20);
+            rotate(45);
+            text(this.data[i][this.xValue], 0, 0);
+            pop();
+        }
+        pop();
+        pop();
+    }
+
+    renderTicks() {
+        push();
+        translate(this.chartPosX, this.chartPosY);
+        noFill();
+        stroke(this.axisColour);
+        strokeWeight(this.axisThickness);
+
+        let tickIncrement = this.chartHeight / this.numTicks;
+
+        fill(this.axisColour);
+        textSize(12);
+        textAlign(RIGHT, CENTER);
+
+        for (let i = 0; i <= this.numTicks; i++) {
+            let y = -tickIncrement * i;
+            line(0, y, -this.tickLength, y); 
+        }
+
+        pop();
+    }
+}
--- CA/sketch.js
+++ CA/sketch.js
@@ -1,13 +1,57 @@
-let data;
+let data; 
+let cleanedData = [];
+let charts = [];
+
 
 function preload(){
     data = loadTable('data/population.csv', 'csv', 'header')
 }
 
 function setup(){
-    createCanvas(600, 600);
+    createCanvas(1000, 1000);
+    angleMode(DEGREES);
+    noLoop();
+    cleanData();
+
+    charts.push(new BarChart({
+        data:cleanedData,
+        xValue:'Year',
+        yValue:'ItalyPop',
+        chartPosX: 50,
+        chartPosY: 350
+        }
+    ))
+
+    //charts.push(new StackedBarChart({
+        //data: cleanedData,
+        //xValue: 'Year',
+        //yValues: ['ItalyPop' , 'WorldPop'],
+        //chartPosX: 400,
+        //chartPosY: 350
+    //}));
 }
+
 
 function draw(){
     background(245, 235, 220);
+
+    charts.forEach(chart => {
+        chart.renderBars();
+        chart.renderLabels();
+        chart.renderAxis();
+        chart.renderTicks();
+    });
 }
+
+function cleanData(){
+    for(let i=0; i<data.rows.length; i++){
+    cleanedData.push(data.rows[i].obj)
+    }
+
+    for(let i=0; i<cleanedData.length; i++){
+        cleanedData[i].ItalyPop = parseInt(cleanedData[i].ItalyPop)
+        cleanedData[i].ItalyUrbanPop = parseInt(cleanedData[i].ItalyUrbanPop)
+        cleanedData[i].WorldPop = parseInt(cleanedData[i].WorldPop)
+    } 
+}
+
